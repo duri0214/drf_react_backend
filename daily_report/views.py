cbf054c99787d741bb2ae1157906f1b8b09f1af4
yoshi <yoshitakaOkada0214@gmail.com>
2023-06-15 20:56:33 +0900

    improvement

--- daily_report/views.py
+++ daily_report/views.py
@@ -1,4 +1,7 @@
-from rest_framework import generics
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, status
+from rest_framework.response import Response
 
 from .models import Daily
 from .serializers import DailyListSerializer, DailyDetailSerializer
@@ -19,3 +22,15 @@ class DailyDetail(generics.RetrieveAPIView):
     """
     queryset = Daily.objects.all()
     serializer_class = DailyDetailSerializer
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.kwargs['pk'])
+
+    def get(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            serializer = self.get_serializer(instance)
+            return Response(serializer.data)
+        except Http404:
+            return Response("404 Not Found", status=status.HTTP_404_NOT_FOUND)
